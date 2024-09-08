c13049efe556e03fd013e7b48bbac783db9f3f91
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:43:54 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,41 @@
+#Assignment 1
+
+#1
+restaurants =  297
+eateries = 256 
+
+#2
+print(restaurants > eateries)
+print(restaurants < eateries)
+
+#3
+if restaurants > eateries:
+    print('restaurants scenery are better than eateries scenery')
+else:
+    print('eateries scenery are better than restaurants scenery')
+    
+if restaurants < eateries:
+    print('eateries serve better foods than restuarants')
+else:
+    print('restaurants serve better foods than eateries')
+ 
+ #4   
+print(bool (restaurants))
+print(bool (eateries))
+
+#Assignment 2
+
+#1
+outfit, dress, attire = ('skirts', 'trousers', 'shirts')
+print(outfit)
+print(dress)
+print(attire)
+
+#2
+coke = fanta = sprite = ['sodas']
+print(coke)
+print(fanta)
+print(sprite)
+
+
+
